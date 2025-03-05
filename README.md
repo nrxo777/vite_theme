cb67f6cd0b47aa76c0904da3306a3283f3e64968
nrxo777 <noorxcalma@gmail.com>
2025-03-06 01:06:57 +0600

    theme_switcher_code

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
-# React + Vite
+<h3>The PROBLEM</h3>
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Some tailwind class problem are still causing some trouble. I'll fix it later.
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { ThemeProvider } from "./context/theme";
+import ThemeBtn from "./components/ThemeBTN";
+import Card from "./components/Card";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  // ******* actual change in theme ******* \\
+
+  let catchTheme = document.querySelector("html").classList;
+  useEffect(() => {
+    catchTheme.remove("light", "dark");
+    catchTheme.add(themeMode);
+  }, [themeMode]);
+
   return (
     <>
-      <div className="flex flex-wrap min-h-screen items-center">
-        <div className="w-full">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
-          {/* Theme BTN*/}
-          <div className="w-full max-w-sm mx-auto"></div>
-          {/* Card */}
+      <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+        <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+            <ThemeBtn />
+            <div className="w-full max-w-sm mx-auto"></div>
+            <Card />
+          </div>
         </div>
-      </div>
+      </ThemeProvider>
     </>
   );
 }
